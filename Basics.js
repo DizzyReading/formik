56b7c1f4402fb4eedcb17580d83abec36f561416
DizzyReading <daintybeat@gmail.com>
2021-08-09 16:16:05 +0530

    add visited fields

--- Basics.js
+++ Basics.js
@@ -49,6 +49,7 @@ const SignupForm = () => {
 				name="firstName"
 				type="text"
 				onChange={formik.handleChange}
+				onBlur={formik.handleBlur}
 				value={formik.values.firstName}
 			/>
 			{formik.errors.firstName ? <div>{formik.errors.firstName}</div> : null}
@@ -59,12 +60,20 @@ const SignupForm = () => {
 				name="lastName"
 				type="text"
 				onChange={formik.handleChange}
+				onBlur={formik.handleBlur}
 				value={formik.values.lastName}
 			/>
 			{formik.errors.lastName ? <div>{formik.errors.lastName}</div> : null}
 
 			<label htmlFor="email">Email Address</label>
-			<input id="email" name="email" type="email" onChange={formik.handleChange} value={formik.values.email} />
+			<input
+				id="email"
+				name="email"
+				type="email"
+				onChange={formik.handleChange}
+				onBlur={formik.handleBlur}
+				value={formik.values.email}
+			/>
 			{formik.errors.email ? <div>{formik.errors.email}</div> : null}
 
 			<button type="submit">Submit</button>
